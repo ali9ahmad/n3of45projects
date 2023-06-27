29ad71d8b92f825325b7a87c4b95b69563b96de5
ali9ahmad <temur.khan.dispatch@gmail.com>
2023-06-28 01:25:33 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const App = () => {
-  return (
-    <div>hello react</div>
-  )
-}
+  
+  const [temperatureValue, setTemperatureValue] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState('cold');
+  
+  const decreaseTemperature = () => {
+    if (temperatureValue === 0) return;
 
-export default App;
+    const newTemperature = temperatureValue - 1;
+    if(newTemperature < 15) {
+      setTemperatureColor('cold');
+    }
+    setTemperatureValue( newTemperature )
+  }
+
+  const increaseTemperature = () => {
+    if (temperatureValue === 30) return;
+
+    const newTemperature = temperatureValue + 1;
+    if(newTemperature >= 15) {
+      setTemperatureColor('hot');
+    }
+    setTemperatureValue( newTemperature )
+  }
+  
+  return (
+    <div className='app-container'>
+    <div className='temperature-display-container'>
+    <div className={`temperature-display ${temperatureColor}`}>{temperatureValue}°C</div>    
+    </div>
+    <div className='button-container'>
+    <button onClick={() => increaseTemperature()}>+</button>
+    <button onClick={() => decreaseTemperature()}>-</button>
+    </div>
+    </div>
+    )
+  }
+  
+  export default App;
